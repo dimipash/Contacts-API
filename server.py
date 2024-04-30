e8b58c83c5dbafc542028ed7a2922ff80dfddd6a
Dimitar Pashev <dim.pashev@gmail.com>
2024-04-30 16:50:17 +0300

    create a list endpoint

--- server.py
+++ server.py
@@ -2,9 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_route():
-    print('I have received a re quest on the /hello endpoint!')
-    return '<h1>Hello from the server!</h1>'
+contacts = [
+    {"id": "1", "name": "Ivan", "phone": "12345678"},
+    {"id": "2", "name": "Georgi", "phone": "22345678"},
+    {"id": "3", "name": "Petar", "phone": "32345678"},
+    {"id": "4", "name": "Misho", "phone": "42345678"},
+]
 
-app.run(debug=True)
+
+@app.get("/contacts")
+def list_contacts():
+    return contacts
+
+
+@app.get("/contacts/<id>")
+def read_single_contact(id):
+    for contact in contacts:
+        if contact["id"] == id:
+            return contact
+
+    return "That contact does not exists"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
